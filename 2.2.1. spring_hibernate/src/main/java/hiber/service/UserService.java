9f3f8f6db791878a23349564f820b285aeaf5c09
Firsdv <firsdv8@gmail.com>
2025-03-21 23:51:47 +0300

    Добавил внедрения через конструктор в UserServiceImp UserDaoImp и AppConfig

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,15 @@
 package hiber.service;
 
+import hiber.dao.UserDao;
 import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 public interface UserService {
+
     void add(User user);
 
     List<User> listUsers();
